fae370224b4bfee732546e48540e90120a2aa4ab
ferz2001 <fmannanov01@gmail.com>
2021-11-12 20:14:18 +0300

    del perm

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,25 +3,11 @@ from rest_framework import permissions
 class IsAdmin(permissions.BasePermission):
 
     def has_permission(self, request, view, obj):
-        return request.user.is_authenticated and request.user.role == 'ADMIN'
+        return request.user.is_authenticated and request.user.rcdole == 'ADMIN'
 
     def has_object_permission(self, request, view, obj):
         return request.user.is_authenticated and request.user.role == 'ADMIN'
 
-class AuthADMMODOrReadOnly(permissions.BasePermission):
-
-    def has_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
-
-    def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or obj.author == request.user or
-            request.user  in ['ADMIN', 'MODERATOR']
-        )
 
 class IsAuthor(permissions.BasePermission):
 
